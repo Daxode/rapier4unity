e016b947f0b8e8011cb9b0964bb7eb191eb8c23c
daniel.andersen <daniel.andersen@unity3d.com>
2025-04-04 04:19:07 +0200

    Fix post main merge

--- source~/rapier-c-bind/build-debug.bat
+++ source~/rapier-c-bind/build-debug.bat
@@ -2,10 +2,11 @@
 REM Build Windows DLLs
 cargo build
 IF NOT EXIST "..\..\build_bin" mkdir "..\..\build_bin"
-copy /Y "target\debug\rapier_c_bind.dll" "..\..\build_bin\"
-copy /Y "target\debug\rapier_c_bind.pdb" "..\..\build_bin\"
-copy /Y "target\debug\unitybridge.dll" "..\..\build_bin\"
-copy /Y "target\debug\unitybridge.pdb" "..\..\build_bin\"
+IF NOT EXIST "..\..\build_bin\Windows" mkdir "..\..\build_bin\Windows"
+copy /Y "target\debug\rapier_c_bind.dll" "..\..\build_bin\Windows\"
+copy /Y "target\debug\rapier_c_bind.pdb" "..\..\build_bin\Windows\"
+copy /Y "target\debug\unitybridge.dll" "..\..\build_bin\Windows\"
+copy /Y "target\debug\unitybridge.pdb" "..\..\build_bin\Windows\"
 cargo run -- ./rapierbind/src
 
 echo Windows debug build completed.
--- source~/rapier-c-bind/build-release.bat
+++ source~/rapier-c-bind/build-release.bat
@@ -2,10 +2,11 @@
 REM Build Windows DLLs in release mode
 cargo build --release
 IF NOT EXIST "..\..\build_bin" mkdir "..\..\build_bin"
-copy /Y "target\release\rapier_c_bind.dll" "..\..\build_bin\"
-copy /Y "target\release\rapier_c_bind.pdb" "..\..\build_bin\"
-copy /Y "target\release\unitybridge.dll" "..\..\build_bin\"
-copy /Y "target\release\unitybridge.pdb" "..\..\build_bin\"
+IF NOT EXIST "..\..\build_bin\Windows" mkdir "..\..\build_bin\Windows"
+copy /Y "target\release\rapier_c_bind.dll" "..\..\build_bin\Windows\"
+copy /Y "target\release\rapier_c_bind.pdb" "..\..\build_bin\Windows\"
+copy /Y "target\release\unitybridge.dll" "..\..\build_bin\Windows\"
+copy /Y "target\release\unitybridge.pdb" "..\..\build_bin\Windows\"
 cargo run -- ./rapierbind/src
 
 echo Windows release build completed.
